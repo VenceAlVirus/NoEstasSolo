02136355525269e3522619941ce595c2d1e95e5a
Yanet Hevia <yanet.hevia@talentomobile.com>
2020-04-04 18:15:36 +0200

    edit profile and my profile fragment

---
+++ app/src/main/java/com/talentoMobile/noEstasSolo/features/perfil/views/EditProfileFragment.kt
@@ -0,0 +1,9 @@
+package com.talentoMobile.noEstasSolo.features.perfil.views
+
+import com.talentoMobile.noEstasSolo.R
+import com.talentoMobile.noEstasSolo.core.platform.BaseFragment
+
+class EditProfileFragment : BaseFragment() {
+    override fun layoutId() = R.layout.fragment_edit_profile
+
+}
---
+++ app/src/main/java/com/talentoMobile/noEstasSolo/features/perfil/views/ProfileFragment.kt
@@ -0,0 +1,9 @@
+package com.talentoMobile.noEstasSolo.features.perfil.views
+
+import com.talentoMobile.noEstasSolo.R
+import com.talentoMobile.noEstasSolo.core.platform.BaseFragment
+
+class ProfileFragment : BaseFragment() {
+    override fun layoutId() = R.layout.fragment_profile
+
+}

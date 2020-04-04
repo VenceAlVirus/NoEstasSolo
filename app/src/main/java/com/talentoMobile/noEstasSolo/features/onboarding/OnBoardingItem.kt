e2e2736cfff987da7154d04d1d91fe05e9441770
Yanet Hevia <yanet.hevia@talentomobile.com>
2020-04-04 17:20:24 +0200

    onBoardingView add some ignoring files

---
+++ app/src/main/java/com/talentoMobile/noEstasSolo/features/onboarding/OnBoardingItem.kt
@@ -0,0 +1,9 @@
+package com.talentoMobile.noEstasSolo.features.onboarding
+
+data class OnBoardingItem(
+    var title: String = "",
+    var description: String = "",
+    var imageID: Int = 0
+)
+
+

f6a9f95adf8c8b25650fb8f3f4e5b14e09cae5b5
Felipe <andresfgdc@hotmail.com>
2020-04-04 16:28:24 +0200

    07-mensajes
    created chat and messages fragments and basic components of each to visualize.

---
+++ app/src/main/java/com/talentoMobile/noEstasSolo/features/chat/views/fragments/ChatFragment.kt
@@ -0,0 +1,9 @@
+package com.talentoMobile.noEstasSolo.features.chat.views.fragments
+
+import com.talentoMobile.noEstasSolo.R
+import com.talentoMobile.noEstasSolo.core.platform.BaseFragment
+
+class ChatFragment : BaseFragment(){
+    override fun layoutId() = R.layout.fragment_chat
+
+}

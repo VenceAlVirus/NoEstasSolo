23b6dba996a04a6557a730d884cdc6db46ec31cd
david <davidarribas.ciclos@gmail.com>
2020-04-04 14:32:48 +0200

    News finish

--- app/src/main/java/com/talentoMobile/noEstasSolo/features/news/views/NewsFragments.kt
+++ app/src/main/java/com/talentoMobile/noEstasSolo/features/news/views/NewsFragments.kt
@@ -1,9 +1,21 @@
 package com.talentoMobile.noEstasSolo.features.news.views
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.findNavController
 import com.talentoMobile.noEstasSolo.R
 import com.talentoMobile.noEstasSolo.core.platform.BaseFragment
+import kotlinx.android.synthetic.main.fragment_news.*
 
 class NewsFragments : BaseFragment() {
     override fun layoutId() = R.layout.fragment_news
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        item.setOnClickListener {
+            view.findNavController().navigate(R.id.action_newsFragment_to_newFragment)
+        }
+    }
 
 }
